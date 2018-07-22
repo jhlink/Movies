f5f845aa0ef07f4761c643b9a2ce98ffb4f12a48
James <jhlink@users.noreply.github.com>
2018-07-22 03:41:13 -0400

    feat: Utilize API key using BuildConfigField with app/build.gradle

--- app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
+++ app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
@@ -2,12 +2,13 @@ package com.udacity.oliverh.movies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("KEY", BuildConfig.MOVIE_API_KEY);
     }
 }
