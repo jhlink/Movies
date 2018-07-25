3b486b271226712fa4f5ffcd8b40c5bdc37493cd
James Kim <jhlink@users.noreply.github.com>
2018-07-25 18:08:41 -0400

    feat: Construct GridView through assembly of RecyclerView components

--- app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
+++ app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
@@ -1,14 +1,31 @@
 package com.udacity.oliverh.movies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NUM_MOVIE_GRID_ITEMS = 20;
+    private static final int NUM_MOVIE_GRID_SPAN_COUNT = 3;
+    private MovieAdapter mAdapter;
+    private RecyclerView mMovieGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("KEY", BuildConfig.MOVIE_API_KEY);
+
+        mMovieGrid = (RecyclerView) findViewById(R.id.rv_movies);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, NUM_MOVIE_GRID_SPAN_COUNT);
+        mMovieGrid.setLayoutManager(layoutManager);
+
+        mMovieGrid.setHasFixedSize(true);
+
+        mAdapter = new MovieAdapter(NUM_MOVIE_GRID_ITEMS);
+
+        mMovieGrid.setAdapter(mAdapter);
+        //Log.i("KEY", BuildConfig.MOVIE_API_KEY);
     }
 }
