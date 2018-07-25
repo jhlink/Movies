28fbfbea2ec9c0e5a84b46d211af93e8a5f9ec3a
James Kim <jhlink@users.noreply.github.com>
2018-07-25 17:41:49 -0400

    feat: Create MovieAdapter class with overriden onCreateViewHolder method

---
+++ app/src/main/java/com/udacity/oliverh/movies/MovieAdapter.java
@@ -0,0 +1,31 @@
+package com.udacity.oliverh.movies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private int mNumberOfMovies;
+
+    public MovieAdapter(int numOfMovies) {
+        mNumberOfMovies = numOfMovies;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
+        Context context = viewGroup.getContext();
+        int layoutIDForGridItem = R.layout.movie_grid_item;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIDForGridItem, viewGroup, shouldAttachToParentImmediately);
+        MovieViewHolder viewHolder = new MovieViewHolder(view);
+
+        return viewHolder;
+    }
+}
