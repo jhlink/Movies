4f4e14124b0082099de7ba49d0fd39a609aa703e
James Kim <jhlink@users.noreply.github.com>
2018-07-27 05:04:08 -0400

    feat: Create DateAdapter for moshi

---
+++ app/src/main/java/com/udacity/oliverh/movies/utilities/MoshiAdapters/DateAdapter.java
@@ -0,0 +1,36 @@
+package com.udacity.oliverh.movies.utilities.MoshiAdapters;
+
+import android.util.Log;
+
+import com.squareup.moshi.FromJson;
+import com.squareup.moshi.JsonDataException;
+import com.squareup.moshi.ToJson;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateAdapter {
+    private final static String DATE_ADAPTER_TAG = DateAdapter.class.getSimpleName();
+    private final static SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+
+    @ToJson String toJson(Date date) {
+        return mformat.format(date);
+    }
+
+    @FromJson Date fromJson(String sDate) {
+        if ( sDate.length() != 10) {
+            throw new JsonDataException("Date JSON Anomaly");
+        }
+
+        Date date = null;
+        try {
+            date = mformat.parse(String.valueOf(sDate));
+        } catch (ParseException e) {
+            Log.e(DATE_ADAPTER_TAG, e.toString());
+        }
+
+        return date;
+    }
+}
