a9078eba1ab094fb2f6824d14fa71b9126bb8c6f
James Kim <jhlink@users.noreply.github.com>
2018-07-27 15:55:14 -0400

    feat: Implement JSON parsing for received JSON response of getPopularMovies request

--- app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
+++ app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Window;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
 import com.udacity.oliverh.movies.model.Movie;
+import com.udacity.oliverh.movies.model.QueriedMovieList;
 import com.udacity.oliverh.movies.utilities.MoshiAdapters.DateAdapter;
 import com.udacity.oliverh.movies.utilities.MovieServiceAPI;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NUM_MOVIE_GRID_ITEMS = 20;
@@ -36,23 +45,38 @@ public class MainActivity extends AppCompatActivity {
 
         mMovieGrid.setAdapter(mAdapter);
 
+        showPopularMovies();
+
+    }
+
+    private void showPopularMovies() {
+        Callback popularMoviesRequestCb = new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                QueriedMovieList movies = jsonListParser( response.body().string() ) ;
+            }
+        };
+
         try {
-            // Need to Create a Singleton helper class to ensure that OkHTTPClient is only
-            //  instantiated once.
-            new MovieServiceAPI().getTopRatedMovies(this);
+            MovieServiceAPI.getPopularMovies(this, popularMoviesRequestCb);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void test() throws IOException {
+    public QueriedMovieList jsonListParser(String jsonResponse) throws IOException {
         Moshi moshi = new Moshi.Builder()
                 .add(new DateAdapter())
                 .build();
-        JsonAdapter<Movie> jsonAdapter = moshi.adapter(Movie.class);
 
-        Log.i("MOSHI", String.valueOf(getString(R.string.jsontest)));
-        Movie testVal = jsonAdapter.fromJson(getString(R.string.jsontest));
-        Log.i("MOSHI", testVal.toString());
+        JsonAdapter<QueriedMovieList> jsonAdapter = moshi.adapter(QueriedMovieList.class);
+
+        QueriedMovieList result = jsonAdapter.fromJson(jsonResponse);
+        return result;
     }
 }
--- app/src/main/java/com/udacity/oliverh/movies/utilities/MovieServiceAPI.java
+++ app/src/main/java/com/udacity/oliverh/movies/utilities/MovieServiceAPI.java
@@ -26,9 +26,9 @@ public class MovieServiceAPI {
     private final static int POPULAR_MOVIES_QUERY_STRING_ID = R.string.POPULAR_MOVIES_QUERY_STRING;
     private final static int TOP_RATED_QUERY_STRING_ID = R.string.TOP_RATED_QUERY_STRING;
 
-    private final OkHttpClient client = OkHttpSingleton.getInstance().getClient();
+    private static final OkHttpClient client = OkHttpSingleton.getInstance().getClient();
 
-    public static void getPopularMovies(Context context) throws IOException {
+    public static void getPopularMovies(Context context, Callback cb) throws IOException {
         HttpUrl builtUri = HttpUrl.parse(context.getString(MOVIE_DB_BASE_URL_ID)).newBuilder()
                 .addPathSegment(context.getString(POPULAR_MOVIES_QUERY_STRING_ID))
                 .addQueryParameter(context.getString(API_KEY_QUERY_STRING_ID), MOVIE_DB_API_KEY)
@@ -38,21 +38,7 @@ public class MovieServiceAPI {
                 .url(builtUri)
                 .build();
 
-        String test;
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                call.cancel();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String jsonResponse = response.body().string();
-
-                Log.i(MOVIE_SERVICE_API_TAG, jsonResponse);
-            }
-        });
+        client.newCall(request).enqueue(cb);
     }
 
     public static void getTopRatedMovies(Context context) throws IOException {
