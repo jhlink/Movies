7f9625af828aa24e7dd260f4df2026050ede6f28
James Kim <jhlink@users.noreply.github.com>
2018-07-27 08:29:43 -0400

    feat: Implement getPopularMovies helper method in MovieServiceAPI

--- app/src/main/java/com/udacity/oliverh/movies/utilities/MovieServiceAPI.java
+++ app/src/main/java/com/udacity/oliverh/movies/utilities/MovieServiceAPI.java
@@ -1,9 +1,22 @@
 package com.udacity.oliverh.movies.utilities;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.udacity.oliverh.movies.BuildConfig;
 import com.udacity.oliverh.movies.R;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MovieServiceAPI {
+    private final static String MOVIE_SERVICE_API_TAG = MovieServiceAPI.class.getSimpleName();
     final static String MOVIE_DB_API_KEY = BuildConfig.MOVIE_API_KEY;
 
     // Resource IDs for strings used to construct Movie URL
@@ -12,11 +25,41 @@ public class MovieServiceAPI {
     final static int POPULAR_MOVIES_QUERY_STRING_ID = R.string.POPULAR_MOVIES_QUERY_STRING;
     final static int TOP_RATED_QUERY_STRING_ID = R.string.TOP_RATED_QUERY_STRING;
 
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void getPopularMovies(Context context) throws IOException {
+        HttpUrl builtUri = HttpUrl.parse(context.getString(MOVIE_DB_BASE_URL_ID)).newBuilder()
+                .addPathSegment(context.getString(POPULAR_MOVIES_QUERY_STRING_ID))
+                .addQueryParameter(context.getString(API_KEY_QUERY_STRING_ID), MOVIE_DB_API_KEY)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(builtUri)
+                .build();
+
+        String test;
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonResponse = response.body().string();
+
+                Log.i(MOVIE_SERVICE_API_TAG, jsonResponse);
+            }
+        });
+
+    }
+
     // Movie posters, most popular, top rated
     //  movie information
-        // orginal title
-        // poster thumbnail
-        // plot synopsis
-        // user rating / vote_average
-        // release date
+    // orginal title
+    // poster thumbnail
+    // plot synopsis
+    // user rating / vote_average
+    // release date
 }
