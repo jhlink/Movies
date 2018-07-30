fe35a48bb0cf977d6b927bc54096cf343ce23c0d
James Kim <jhlink@users.noreply.github.com>
2018-07-30 03:20:13 -0400

    feat: Implement ProgressBar and TextView error message functionality

--- app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
+++ app/src/main/java/com/udacity/oliverh/movies/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
@@ -24,11 +27,13 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
-    implements  MovieAdapter.GridItemClickListener {
+        implements MovieAdapter.GridItemClickListener {
     private static final int NUM_MOVIE_GRID_ITEMS = 20;
     private static final int NUM_MOVIE_GRID_SPAN_COUNT = 3;
     private MovieAdapter mAdapter;
     private RecyclerView mMovieGrid;
+    private ProgressBar mProgressBar;
+    private TextView mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mMovieGrid = (RecyclerView) findViewById(R.id.rv_movies);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        mErrorMessage = (TextView) findViewById(R.id.tv_error_message);
 
         GridLayoutManager layoutManager = new GridLayoutManager(this, NUM_MOVIE_GRID_SPAN_COUNT);
         mMovieGrid.setLayoutManager(layoutManager);
@@ -48,6 +55,25 @@ public class MainActivity extends AppCompatActivity
 
         showTopRatedMovies();
     }
+
+    private void onNetworkFailure() {
+        mErrorMessage.setVisibility(View.VISIBLE);
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mMovieGrid.setVisibility(View.INVISIBLE);
+    }
+
+    private void onNetworkRequest() {
+        mErrorMessage.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
+        mMovieGrid.setVisibility(View.INVISIBLE);
+    }
+
+    private void onNetworkSuccess() {
+        mMovieGrid.setVisibility(View.VISIBLE);
+        mErrorMessage.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
 
     @Override
     public void onGridItemClick(Movie movie) {
@@ -68,7 +94,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onOptionsItemSelected(MenuItem item) {
         int selectedMenuItem = item.getItemId();
 
-        switch ( selectedMenuItem ) {
+        switch (selectedMenuItem) {
             case R.id.action_popular:
                 showPopularMovies();
                 return true;
@@ -88,17 +114,24 @@ public class MainActivity extends AppCompatActivity
         Callback topRatedMoviesRequestCb = new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onNetworkFailure();
+                    }
+                });
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                final QueriedMovieList movies = jsonListParser( response.body().string() ) ;
+
+                final QueriedMovieList movies = jsonListParser(response.body().string());
                 expandMovieImageUrls(movies.getResults());
 
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        onNetworkSuccess();
                         mAdapter.setMovieListData(movies.getResults());
                         mAdapter.notifyDataSetChanged();
                     }
@@ -107,6 +140,7 @@ public class MainActivity extends AppCompatActivity
         };
 
         try {
+            onNetworkRequest();
             MovieServiceAPI.getTopRatedMovies(this, topRatedMoviesRequestCb);
         } catch (IOException e) {
             e.printStackTrace();
@@ -117,17 +151,23 @@ public class MainActivity extends AppCompatActivity
         Callback popularMoviesRequestCb = new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onNetworkFailure();
+                    }
+                });
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                final QueriedMovieList movies = jsonListParser( response.body().string() ) ;
+                final QueriedMovieList movies = jsonListParser(response.body().string());
                 expandMovieImageUrls(movies.getResults());
 
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        onNetworkSuccess();
                         mAdapter.setMovieListData(movies.getResults());
                         mAdapter.notifyDataSetChanged();
                     }
@@ -136,6 +176,7 @@ public class MainActivity extends AppCompatActivity
         };
 
         try {
+            onNetworkRequest();
             MovieServiceAPI.getPopularMovies(this, popularMoviesRequestCb);
         } catch (IOException e) {
             e.printStackTrace();
@@ -143,7 +184,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void expandMovieImageUrls(List<Movie> movies) {
-        for ( int i = 0; i < movies.size(); i++ ) {
+        for (int i = 0; i < movies.size(); i++) {
             String newString = MovieServiceAPI.getMoviePosterUrl(MainActivity.this,
                     "w185",
                     movies.get(i).getPosterPath().substring(1));
