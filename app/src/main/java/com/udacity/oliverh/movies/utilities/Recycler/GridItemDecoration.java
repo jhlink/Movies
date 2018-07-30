4d6934890996a0f06640c7c7ce98ca1dfc0c7544
James Kim <jhlink@users.noreply.github.com>
2018-07-30 05:24:36 -0400

    feat: Create GridItemDecoration class

--- app/src/main/java/com/udacity/oliverh/movies/utilities/Recycler/GridItemDecoration.java
+++ app/src/main/java/com/udacity/oliverh/movies/utilities/Recycler/GridItemDecoration.java
@@ -1,6 +1,32 @@
 package com.udacity.oliverh.movies.utilities.Recycler;
 
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class GridItemDecoration extends RecyclerView.ItemDecoration {
+    private int mSpace;
+
+    public GridItemDecoration(int space) {
+        this.mSpace = space;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect,
+                               @NonNull View view,
+                               @NonNull RecyclerView parent,
+                               @NonNull RecyclerView.State state) {
+
+        outRect.left = mSpace;
+        outRect.right = 0;
+        outRect.bottom = mSpace;
+
+        if (parent.getChildLayoutPosition(view) == 0
+                || parent.getChildLayoutPosition(view) == 1) {
+            outRect.top = mSpace;
+        } else {
+            outRect.top = 0;
+        }
+    }
 }
