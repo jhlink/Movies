52595919484f02ad159c630f7c7e2f5b1c33c576
James Kim <jhlink@users.noreply.github.com>
2018-10-09 11:20:51 -0400

    feat: Create Movie Database Access Object interface with loadAllFavoriteMovies, insertMovie, and deleteMovie functions

---
+++ app/src/main/java/com/udacity/oliverh/movies/data/database/MovieDao.java
@@ -0,0 +1,21 @@
+package com.udacity.oliverh.movies.data.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao {
+
+   @Query("SELECT * FROM movie")
+   List<Movie> loadAllFavoriteMovies();
+
+   @Insert
+   void insertMovie(Movie movie);
+
+   @Delete
+   void deleteMovie(Movie movie);
+}
