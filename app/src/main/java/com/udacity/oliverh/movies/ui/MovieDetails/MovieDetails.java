49fdebbd3b7402e05812eedc990c093a4722f51d
James Kim <jhlink@users.noreply.github.com>
2018-10-12 05:33:37 -0400

    feat: Integreate viewModel and viewModelFactory for MovieDetails

--- app/src/main/java/com/udacity/oliverh/movies/ui/MovieDetails/MovieDetails.java
+++ app/src/main/java/com/udacity/oliverh/movies/ui/MovieDetails/MovieDetails.java
@@ -1,8 +1,12 @@
 package com.udacity.oliverh.movies.ui.MovieDetails;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.udacity.oliverh.movies.R;
@@ -32,6 +36,19 @@ public class MovieDetails extends AppCompatActivity {
             binding.setVariable(movie, movieData);
             binding.executePendingBindings();
         }
+    }
+
+    private void setupViewModelFactory(int movieId) {
+        MovieDetailsViewModelFactory factory = new MovieDetailsViewModelFactory(mDb, movieId);
+        final MovieDetailsViewModel viewModel = ViewModelProviders.of(this, factory).get(MovieDetailsViewModel.class);
+
+        viewModel.getMovie().observe(this, new Observer<Movie>() {
+            @Override
+            public void onChanged(@Nullable Movie movie) {
+                viewModel.getMovie().removeObserver(this);
+                // Use binding to set live updates?
+            }
+        });
     }
 
 
