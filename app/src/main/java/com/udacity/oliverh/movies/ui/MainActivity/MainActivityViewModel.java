8db30d4cd7ae1d50bfcb68bc729055db6b096e36
James Kim <jhlink@users.noreply.github.com>
2018-10-18 15:35:41 -0400

    test: Add log messages for MainActivityViewModel

--- app/src/main/java/com/udacity/oliverh/movies/ui/MainActivity/MainActivityViewModel.java
+++ app/src/main/java/com/udacity/oliverh/movies/ui/MainActivity/MainActivityViewModel.java
@@ -8,6 +8,7 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.udacity.oliverh.movies.data.MovieRepository;
 import com.udacity.oliverh.movies.data.database.AppDatabase;
@@ -17,6 +18,8 @@ import com.udacity.oliverh.movies.data.network.ApiResponse;
 import java.util.List;
 
 public class MainActivityViewModel extends AndroidViewModel {
+
+    private static final String TAG = MainActivityViewModel.class.getSimpleName();
     private final MovieRepository mRepository;
     //private final LiveData<List<Movie>> mMovies;
     private final MediatorLiveData<ApiResponse> movieApiResponse;
@@ -29,10 +32,12 @@ public class MainActivityViewModel extends AndroidViewModel {
     }
 
     public LiveData<ApiResponse> getTopRatedMovies() {
+        Log.d(TAG, "Request TopRatedMovies from MainActivityViewModel");
         movieApiResponse.addSource(mRepository.getTopRatedMovies(this.getApplication().getApplicationContext()),
                 new Observer<ApiResponse>() {
                     @Override
                     public void onChanged(@Nullable ApiResponse apiResponse) {
+                        Log.d(TAG, "Update TopRatedMovies list");
                         movieApiResponse.setValue(apiResponse);
                     }
                 });
