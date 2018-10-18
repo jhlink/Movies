2787edb0f8e513d60dad17e6c5ad5298e9d4e6f3
James Kim <jhlink@users.noreply.github.com>
2018-10-18 14:26:53 -0400

    feat: Implement getTopRatedMovies network w/ error handling query in MovieRepository

--- app/src/main/java/com/udacity/oliverh/movies/data/MovieRepository.java
+++ app/src/main/java/com/udacity/oliverh/movies/data/MovieRepository.java
@@ -2,14 +2,29 @@ package com.udacity.oliverh.movies.data;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
 import com.udacity.oliverh.movies.AppExecutors;
 import com.udacity.oliverh.movies.data.database.AppDatabase;
 import com.udacity.oliverh.movies.data.database.Movie;
 import com.udacity.oliverh.movies.data.database.MovieDao;
+import com.udacity.oliverh.movies.data.database.QueriedMovieList;
+import com.udacity.oliverh.movies.data.network.ApiResponse;
+import com.udacity.oliverh.movies.data.network.MoshiAdapters.DateAdapter;
+import com.udacity.oliverh.movies.data.network.MovieServiceAPI;
+import com.udacity.oliverh.movies.ui.MainActivity.MainActivity;
 
+import java.io.IOException;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MovieRepository {
 
@@ -50,5 +65,43 @@ public class MovieRepository {
                 mMovieDao.insertMovie(movie);
             }
         });
+    }
+
+    public LiveData<ApiResponse> getTopRatedMovies(final Context context) {
+        final MutableLiveData<ApiResponse> movieApiResponse = new MutableLiveData<>();
+
+        AppExecutors.getInstance().networkIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                Call topRatedMovieCall = MovieServiceAPI.getTopRatedMovies(context);
+                topRatedMovieCall.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        movieApiResponse.postValue(new ApiResponse(e));
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final QueriedMovieList movies = jsonListParser(response.body().string());
+                            ApiResponse successfulResponse = new ApiResponse(movies.getResults());
+                            movieApiResponse.postValue(successfulResponse);
+                        }
+                    }
+                });
+            }
+        });
+
+        return movieApiResponse;
+    }
+
+    private QueriedMovieList jsonListParser(String jsonResponse) throws IOException {
+        Moshi moshi = new Moshi.Builder()
+                .add(new DateAdapter())
+                .build();
+
+        JsonAdapter<QueriedMovieList> jsonAdapter = moshi.adapter(QueriedMovieList.class);
+
+        return jsonAdapter.fromJson(jsonResponse);
     }
 }
