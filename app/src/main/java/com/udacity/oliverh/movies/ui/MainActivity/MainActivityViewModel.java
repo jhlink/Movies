e1baadc22cd123cefdc7689a6a70e1e7a3a1d2e3
James Kim <jhlink@users.noreply.github.com>
2018-10-19 11:17:51 -0400

    fix: Remove previous LiveData source when requesting new data

--- app/src/main/java/com/udacity/oliverh/movies/ui/MainActivity/MainActivityViewModel.java
+++ app/src/main/java/com/udacity/oliverh/movies/ui/MainActivity/MainActivityViewModel.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.udacity.oliverh.movies.data.AbsentLiveData;
 import com.udacity.oliverh.movies.data.MovieRepository;
 import com.udacity.oliverh.movies.data.database.AppDatabase;
 import com.udacity.oliverh.movies.data.database.Movie;
@@ -23,26 +24,32 @@ public class MainActivityViewModel extends AndroidViewModel {
     private static final String TAG = MainActivityViewModel.class.getSimpleName();
     private final MovieRepository mRepository;
     private final MediatorLiveData<RepositoryResponse> movieApiResponse;
+    private LiveData<RepositoryResponse> previousLiveData;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         AppDatabase database = AppDatabase.getInstance(application);
         mRepository = MovieRepository.getInstance(database);
         movieApiResponse = new MediatorLiveData<>();
+        previousLiveData = null;
         fetchTopRatedMovies(application.getApplicationContext());
     }
 
     public void fetchTopRatedMovies(Context mContext) {
         Log.d(TAG, "Fetch TopRatedMovies -> MovieRepository");
-        setSourceForLiveData(mRepository.getTopRatedMovies(mContext));
+        removePreviousSource();
+        previousLiveData = mRepository.getTopRatedMovies(mContext);
+        setSourceForLiveData(previousLiveData);
     }
 
     public void fetchPopularMovies(Context mContext) {
         Log.d(TAG, "Fetch PopularMovies -> MovieRepository");
-        setSourceForLiveData(mRepository.getPopularMovies(mContext));
+        removePreviousSource();
+        previousLiveData = mRepository.getPopularMovies(mContext);
+        setSourceForLiveData(previousLiveData);
     }
 
-    private void setSourceForLiveData(LiveData<RepositoryResponse> movieList) {
+    private void setSourceForLiveData(final LiveData<RepositoryResponse> movieList) {
         movieApiResponse.addSource(movieList,
                 new Observer<RepositoryResponse>() {
                     @Override
@@ -53,6 +60,13 @@ public class MainActivityViewModel extends AndroidViewModel {
                 });
     }
 
+    private void removePreviousSource() {
+        if ( previousLiveData == null ) {
+            return;
+        }
+        movieApiResponse.removeSource(previousLiveData);
+    }
+
     public LiveData<RepositoryResponse> getData() {
         Log.d(TAG, "Get movieList Data");
         return movieApiResponse;
@@ -60,6 +74,8 @@ public class MainActivityViewModel extends AndroidViewModel {
 
     public void fetchFavoriteMovies() {
         Log.d(TAG, "Fetch FavoriteMovies -> MovieRepository");
-        setSourceForLiveData(mRepository.getFavoriteMovies());
+        removePreviousSource();
+        previousLiveData = mRepository.getFavoriteMovies();
+        setSourceForLiveData(previousLiveData);
     }
 }
