211360ef910cec110b0905e2f58b8be62dd2bcfc
James Kim <jhlink@users.noreply.github.com>
2018-10-22 17:43:31 -0400

    feat: Create MovieVideo POJO class for Moshi

---
+++ app/src/main/java/com/udacity/oliverh/movies/data/network/MovieVideo.java
@@ -0,0 +1,49 @@
+package com.udacity.oliverh.movies.data.network;
+
+public class MovieVideo {
+    private String videoId;
+    private String key;
+    private String name;
+    private String site;
+    private String type;
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+}
