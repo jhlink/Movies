f41c3ecbd69b90eb4998f43580d6d1f77c2d22aa
James Kim <jhlink@users.noreply.github.com>
2018-10-23 01:49:32 -0400

    feat: Implement Intent to launch trailer from image button in VideoAdapter

--- app/src/main/java/com/udacity/oliverh/movies/ui/MovieDetails/Recycler/VideoAdapter.java
+++ app/src/main/java/com/udacity/oliverh/movies/ui/MovieDetails/Recycler/VideoAdapter.java
@@ -1,11 +1,15 @@
 package com.udacity.oliverh.movies.ui.MovieDetails.Recycler;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.udacity.oliverh.movies.R;
 import com.udacity.oliverh.movies.data.network.MovieVideo;
@@ -19,9 +23,11 @@ import static com.udacity.oliverh.movies.BR.movieVideo;
 public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MovieVideoViewHolder> {
     private List<MovieVideo> movieVideos = new ArrayList<>();
 
-    class MovieVideoViewHolder extends RecyclerView.ViewHolder {
+    class MovieVideoViewHolder extends RecyclerView.ViewHolder
+    implements ImageButton.OnClickListener {
 
         private final VideoListItemBinding videoListItemBinding;
+        private final String TAG = MovieVideoViewHolder.class.getSimpleName();
 
         public MovieVideoViewHolder(VideoListItemBinding binding) {
             super(binding.getRoot());
@@ -31,7 +37,23 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MovieVideoVi
 
         void bind(MovieVideo iVideo) {
             videoListItemBinding.setVariable(movieVideo, iVideo);
+            videoListItemBinding.imageButton.setOnClickListener(this);
             videoListItemBinding.executePendingBindings();
+        }
+
+        @Override
+        public void onClick(View v) {
+            MovieVideo clickedMovieVideo = movieVideos.get(getAdapterPosition());
+            String youtubeVideoKey = clickedMovieVideo.getKey();
+            Intent launchYoutubeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + youtubeVideoKey));
+
+            Context context = v.getContext();
+            if (launchYoutubeIntent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(launchYoutubeIntent);
+                Log.d(TAG, "Intent to launch Youtube application succeeded");
+            } else {
+                Log.d(TAG, "Intent to launch Youtube application failed");
+            }
         }
     }
 
